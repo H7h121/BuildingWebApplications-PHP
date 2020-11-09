4fcf9abcd6b4b41ce430566a5b25183b0a68442e
Hadi Haidar <73796565+H7h121@users.noreply.github.com>
2020-11-09 15:21:27 +0200

    Add files via upload

---
+++ BuildingWebApplications-PHP/Week 5/index.php
@@ -0,0 +1,19 @@
+
+<title>Hadi Haidar PHP</title>
+<h1>Hadi Haidar PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Hadi Haidar\" is ".hash('sha256', 'Hadi Haidar');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
